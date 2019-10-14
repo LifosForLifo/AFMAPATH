98ed4c5080a8fb84aafbf59515d961b4498ab8c5
abdelf1006 <abdelmogitfoham@gmail.com>
2019-10-14 20:47:56 +0100

    add form

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -11,63 +11,24 @@
           <div class="col-lg-8">
             <form
               class="form-contact contact_form"
-              action="contact_process.php"
-              method="post"
-              id="contactForm"
-              novalidate="novalidate"
+              name="contact"
+              method="POST"
+              data-netlify="true"
             >
               <div class="row">
                 <div class="col-sm-6">
                   <div class="form-group">
-                    <input
-                      class="form-control"
-                      name="name"
-                      id="name"
-                      type="text"
-                      onfocus="this.placeholder = ''"
-                      onblur="this.placeholder = 'Nom'"
-                      placeholder="Nom"
-                    />
+                    <input class="form-control" name="name" id="name" type="text" />
                   </div>
                 </div>
                 <div class="col-sm-6">
                   <div class="form-group">
-                    <input
-                      class="form-control"
-                      name="email"
-                      id="email"
-                      type="email"
-                      onfocus="this.placeholder = ''"
-                      onblur="this.placeholder = 'Adresse mail'"
-                      placeholder="Adresse mail"
-                    />
+                    <input class="form-control" name="email" type="email" />
                   </div>
                 </div>
                 <div class="col-12">
                   <div class="form-group">
-                    <input
-                      class="form-control"
-                      name="subject"
-                      id="subject"
-                      type="text"
-                      onfocus="this.placeholder = ''"
-                      onblur="this.placeholder = 'Objet'"
-                      placeholder="Objet"
-                    />
-                  </div>
-                </div>
-                <div class="col-12">
-                  <div class="form-group">
-                    <textarea
-                      class="form-control w-100"
-                      name="message"
-                      id="message"
-                      cols="30"
-                      rows="9"
-                      onfocus="this.placeholder = ''"
-                      onblur="this.placeholder = 'Message'"
-                      placeholder="Message"
-                    ></textarea>
+                    <textarea class="form-control w-100" name="message" cols="30" rows="9"></textarea>
                   </div>
                 </div>
               </div>
